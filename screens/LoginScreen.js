1f0ac78476b14e7fc63d10fffd0d69d8488210fb
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-01 07:03:39 +0100

    chore: update package.json

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,24 @@
-import AuthContent from '../components/Auth/AuthContent';
+import { useState } from "react";
+import AuthContent from "../components/Auth/AuthContent";
+import { loginApi } from "../utils/auth";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleLogin = async ({ email, password }) => {
+    setIsLoading(true);
+
+    await loginApi(email, password);
+
+    setIsLoading(false);
+  };
+
+  if (isLoading) {
+    return <LoadingOverlay message="Logging you in" />;
+  }
+
+  return <AuthContent isLogin onAuthenticate={handleLogin} />;
 }
 
 export default LoginScreen;
