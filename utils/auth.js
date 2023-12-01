491cdb938b6ee8882131238c1685c7d1ac14888a
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-01 06:30:42 +0100

    chore: update auth container

--- utils/auth.js
+++ utils/auth.js
@@ -5,14 +5,24 @@ import axios from "axios";
 const authenicate = async (mode, email, password) => {
   const url = `${BASE_URL}${mode}?key=${API_KEY}`;
 
-  const res = await axios.post(url, { email, password });
+  const res = await axios.post(url, {
+    email,
+    password,
+    returnSecureToken: true,
+  });
 
   const { idToken: token } = res.data;
   return token;
 };
 
-const signupApi = async (email, password) => {
+export const signupApi = async (email, password) => {
   const data = await authenicate("signUp", email, password);
+
+  return data;
+};
+
+export const loginApi = async (email, password) => {
+  const data = await authenicate("signInWithPasssword", email, password);
 
   return data;
 };
