3d94a852d27c576975bf215f234f91e7822b97cc
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-01 17:46:26 +0100

    chore: add authentication to signup screen

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import AuthContent from "../components/Auth/AuthContent";
 import { signupApi } from "../utils/auth";
+import { Alert } from "react-native";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
 
 function SignupScreen() {
+  const [isAuthenticating, setISAuthenticating] = useState(false);
+
   const handleSignup = async ({ email, password }) => {
-    await signupApi(email, password);
+    try {
+      setISAuthenticating(true);
+
+      await signupApi(email, password);
+    } catch (error) {
+      Alert.alert(
+        "Authentication failed",
+        "Please check your inputs values or try again later "
+      );
+    }
+
+    setISAuthenticating(false);
   };
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="creating user..." />;
+  }
 
   return <AuthContent onAuthenticate={handleSignup} />;
 }
