8d71780db65ba87a41c402741250854829f4f7b1
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-01 17:43:12 +0100

    chore: add authentication to login screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -2,20 +2,28 @@ import { useState } from "react";
 import AuthContent from "../components/Auth/AuthContent";
 import { loginApi } from "../utils/auth";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { Alert } from "react-native";
 
 function LoginScreen() {
   const [isLoading, setIsLoading] = useState(false);
 
   const handleLogin = async ({ email, password }) => {
-    setIsLoading(true);
+    try {
+      setIsLoading(true);
 
-    await loginApi(email, password);
+      await loginApi(email, password);
+    } catch (error) {
+      Alert.alert(
+        "Authentication failed",
+        "Could not log you in, please check your credentials or try again later"
+      );
+    }
 
     setIsLoading(false);
   };
 
   if (isLoading) {
-    return <LoadingOverlay message="Logging you in" />;
+    return <LoadingOverlay message="Logging you in..." />;
   }
 
   return <AuthContent isLogin onAuthenticate={handleLogin} />;
