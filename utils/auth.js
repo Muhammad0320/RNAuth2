31939ad173fe09b2ab784a259d7b1878dd598f8c
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-01 06:27:11 +0100

    chore: add signup and authenicate functions

---
+++ utils/auth.js
@@ -0,0 +1,18 @@
+import { API_KEY, BASE_URL } from "./constants";
+
+import axios from "axios";
+
+const authenicate = async (mode, email, password) => {
+  const url = `${BASE_URL}${mode}?key=${API_KEY}`;
+
+  const res = await axios.post(url, { email, password });
+
+  const { idToken: token } = res.data;
+  return token;
+};
+
+const signupApi = async (email, password) => {
+  const data = await authenicate("signUp", email, password);
+
+  return data;
+};
